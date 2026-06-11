b239710a959f54670633a4c36fc8eecf545a78c8
andyrids <24698399-andyrids@users.noreply.gitlab.com>
2026-06-11 17:43:44 +0100

    fix: update standards configuration file prefix

--- src/pkgdev/standards/__init__.py
+++ src/pkgdev/standards/__init__.py
@@ -1,10 +1,10 @@
-""""""
+"""Initialise the standards package."""
 
 from pathlib import Path
 
 PKG_ROOT: Path = Path(__file__).parent
 MYPY_CONFIG: Path = PKG_ROOT / "mypy.ini"
-PYMARKDOWN_CONFIG: Path = PKG_ROOT / "pymarkdown.ini"
-RUFF_CONFIG: Path = PKG_ROOT / "ruff.ini"
+PYMARKDOWN_CONFIG: Path = PKG_ROOT / "pymarkdown.toml"
+RUFF_CONFIG: Path = PKG_ROOT / "ruff.toml"
 
 __all__: list[str] = ["MYPY_CONFIG", "PYMARKDOWN_CONFIG", "RUFF_CONFIG"]
